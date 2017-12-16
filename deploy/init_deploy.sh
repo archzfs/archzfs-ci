6a6682927e80524a2463d6bd913c51a670c7477d
Jan Houben <jan@et.tc>
2017-12-16 22:19:46 +0100

    Support for gpg keys with passphrase

--- deploy/init_deploy.sh
+++ deploy/init_deploy.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# only continue, if inside a container
+if [[ ! -f /.dockerenv ]]; then
+    echo 'Not running inside docker!'
+    exit 1
+fi
+
 errors=false
 if [[ ! -f /worker/secrets/ssh_key ]]; then
     echo 'deploy/secrets/ssh_key missing'
@@ -26,14 +32,33 @@ cp /worker/secrets/ssh_server_hostkeys /home/buildbot/.ssh/known_hosts
 chown -R buildbot:buildbot /home/buildbot/.ssh && \
 chmod -R go-rwx /home/buildbot/.ssh
 
-# add gpg key
-mkdir -p /home/buildbot/.gpg
-cp /worker/secrets/gpg_key /home/buildbot/.gpg/gpg_key
-chown -R buildbot:buildbot /home/buildbot/.gpg && \
-chmod -R go-rwx /home/buildbot/.gpg
-su -s /bin/sh buildbot -c "
-gpg --import ~/.gpg/gpg_key
-"
+# gpg
+mkdir -p /home/buildbot/.gnupg
+cp /worker/secrets/gpg_key /home/buildbot/.gnupg/gpg_key
+chown -R buildbot:buildbot /home/buildbot/.gnupg
+chmod -R go-rwx /home/buildbot/.gnupg
+
+# import gpg secret key and remove passphrase
+su buildbot <<EOF
+gpg-agent --daemon --pinentry-program /usr/local/bin/pinentry-auto
+gpg --batch --import ~/.gnupg/gpg_key
+key=\$(gpg --list-keys --with-colons | awk -F: '/^pub:/ { print \$5 }')
+
+# remove gpg passphrase
+/usr/bin/expect <<EOD
+spawn bash -c "gpg --pinentry-mode loopback --batch --command-fd 0 --status-fd 2 --change-passphrase \$key"
+expect "*?passphrase.enter"
+log_user 0
+send "$GPG_PASSPHRASE\r"
+expect "*?passphrase.enter"
+log_user 1
+send "\r"
+expect "*?passphrase.enter"
+send "\r"
+expect eof
+EOD
+
+EOF
 
 # start this worker
 exec /start.sh
